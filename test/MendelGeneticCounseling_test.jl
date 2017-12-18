e8421182cddfabc08d8394f76f5650ae1eedb0f5
benjamin chu <benchu99@hotmail.com>
2017-12-17 22:29:59 -0800

    Starting unit tests

---
+++ test/MendelGeneticCounseling_test.jl
@@ -0,0 +1,8 @@
+using MendelBase                        # From package MendelBase.
+using SearchSetup                       # From package Search.
+using DataFrames                        # From package DataFrames.
+using MendelGeneticCounseling
+
+@testset "basic and wrappers" begin
+    
+end
